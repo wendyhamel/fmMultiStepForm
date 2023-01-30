dcff43ef08e88c2f7efe6b789555ce4d93604c19
wendyhamel <whamel@it4web.nl>
2023-01-30 11:59:06 +0100

    Setting up structure

---
+++ src/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+	theme: {
+		fontFamily : {
+			'sans': ['Ubuntu', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				'blue-gray-light': 'hsl(240, 5%, 91%)', // Divider
+				'blue': 'hsl(240, 73%, 65%)', // Gradient
+				'blue-gray-dark': 'hsl(240, 6%, 50%)', // Text
+				'blue-gray-darker': 'hsl(237, 12%, 33%)', // Text
+				'blue-gray-darkest': 'hsl(238, 29%, 16%)', // Text
+				'soft-red': 'hsl(14, 88%, 65%)', // Text
+				'violet': 'hsl(273, 75%, 66%)', // Gradient
+			},
+		}
+	}
+}
