1b7a68e251f01b814d9f14d9b0533bdd30118cde
wendyhamel <whamel@it4web.nl>
2023-01-30 17:38:09 +0100

    objects to array's.

--- src/stepsForm.js
+++ src/stepsForm.js
@@ -1,32 +1,32 @@
 window.multiSteps = function () {
     return {
-        steps : Alpine.$persist([
-            {id: 1, title: 'Your info', completed: false},
-            {id: 2, title: 'Select plan', completed: false},
-            {id: 3, title: 'Add-ons', completed: false},
-            {id: 4, title: 'Summary', completed: false},
-        ]),
-        plans : Alpine.$persist([
-            {id: 1, name: 'Arcade', priceMonthly : 9, priceYearly : 90, yearlyDiscountDetails: '2 months free', selected: false},
-            {id: 2, name: 'Advanced', priceMonthly : 12, priceYearly : 120, yearlyDiscountDetails: '2 months free', selected: false},
-            {id: 3, name: 'Pro', priceMonthly : 15, priceYearly : 150, yearlyDiscountDetails: '2 months free', selected: false},
-        ]),
-        addons : Alpine.$persist([
-            {id: 1, name: 'Your info', details: 'Your info', priceMonthly : null, priceYearly : null, selected: false},
-            {id: 2, name: 'Select plan', details: 'Select plan', priceMonthly : null, priceYearly : null, selected: false},
-            {id: 3, name: 'Add-ons', details: 'Add-ons', priceMonthly : null, priceYearly : null, selected: false},
-        ]),
+        steps :[
+            [id = 1, title = 'Your info', completed = false],
+            [id = 2, title = 'Select plan', completed = false],
+            [id = 3, title = 'Add-ons', completed = false],
+            [id = 4, title = 'Summary', completed = false],
+        ],
+        plans : [
+            [id = 1, name = 'Arcade', priceMonthly = 9, priceYearly = 90, yearlyDiscountDetails = '2 months free', selected = false],
+            [id = 2, name = 'Advanced', priceMonthly = 12, priceYearly = 120, yearlyDiscountDetails = '2 months free', selected = false],
+            [id = 3, name = 'Pro', priceMonthly = 15, priceYearly = 150, yearlyDiscountDetails = '2 months free', selected = false],
+        ],
+        addons : [
+            [id = 1, name = 'Your info', details = 'Your info', priceMonthly = null, priceYearly  = null, selected = false],
+            [id = 2, name = 'Select plan', details = 'Select plan', priceMonthly = null, priceYearly  = null, selected = false],
+            [id = 3, name = 'Add-ons', details = 'Add-ons', priceMonthly = null, priceYearly  = null, selected = false],
+        ],
         billingYearly: false,
-        currentStep : 2,
-        order : {
-            name: '',
-            email: '',
-            phone: '',
-            plan: {name: '', details: '', price: null},
-            billingYearly: false,
-            addons: [],
-            total: null
-        },
+        currentStep : 1,
+        order : Alpine.$persist([
+            name = '',
+            email = '',
+            phone = '',
+            plan = [name = '', details = '', price = null],
+            billingYearly = false,
+            addons = [],
+            total = null
+        ]),
 
     }
 }
